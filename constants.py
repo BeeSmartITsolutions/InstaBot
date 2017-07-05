0ad9170af8a6abf9f14e8d73c0715ad73959e1d5
Mohit Thakur <mohitthakur.buest@gmail.com>
2017-07-05 21:05:47 +0530

    Global Variable File initiated.

---
+++ constants.py
@@ -0,0 +1,6 @@
+# global variables file
+
+# instagram API access token
+APP_ACCESS_TOKEN = '2114661144.9cfe800.54c6ab35d38f495e83553b03ba50b726'
+# instagram API base url
+BASE_URL = 'https://api.instagram.com/v1/'
