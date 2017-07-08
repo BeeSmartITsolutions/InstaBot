2a4c2091977bcfef18b2e5226e5824560f26c1be
Mohit Thakur <mohitthakur.buest@gmail.com>
2017-07-08 21:36:07 +0530

    self_info.py and instabot.py files are updated

--- instabot.py
+++ instabot.py
@@ -1,4 +1,15 @@
 # instabot.py file created
 from self_info import self_info
 #fetching own informaation
-self_info()
+
+print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+print '\t\tWelcome to InstaBot'
+print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+
+answer = raw_input('Enter your choice: \n1.\tShow your own profile Details \n2.\tSearch a User by its Username 3.\nExit')
+
+if len(answer) > 0 :
+    if answer == 1:
+        self_info()
+    if answer == 2:
+        get_user_id()
--- self_info.py
+++ self_info.py
@@ -6,4 +6,14 @@ def self_info():
     request_url = (BASE_URL + 'users/self/?access_token= %s') % (APP_ACCESS_TOKEN)
     print 'GET request url :- %s' % (request_url)
     user_info = requests.get(request_url).json()
-    print user_info
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print '\nUsername is : %s' % (user_info['data']['username'])
+            print '\nNumber of people who are following you are : %s' % (user_info['data']['counts']['followed_by'])
+            print '\nNumber of people whom you are following are : %s' % (user_info['data']['counts']['follows'])
+            print '\nNumber of posts you have uploaded till now are : %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'User does not exist'
+    else:
+        print 'status code received is other than 200'
